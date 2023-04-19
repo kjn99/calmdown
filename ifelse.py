455839967760d8e8689b20c15424d5130b4f50b5
kjn99 <119350430+kjn99@users.noreply.github.com>
2023-04-20 01:39:04 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+hot = True
+cold = False
+
+if hot:
+    print("It's a hot day")
+    print("drink plenty of water")
+
+elif cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
